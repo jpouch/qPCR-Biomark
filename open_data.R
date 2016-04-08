fffa2ddcfbe96268054fc05ec89c9b393479eb02
Juliette Pouch <pouch@biologie.ens.fr>
2016-04-08 11:42:16 +0200

    Update open_data.R

--- open_data.R
+++ open_data.R
@@ -13,32 +13,18 @@
 #' Use "NULL" for columns that you do not want to use.
 #' @param na.string : change all the 999 to NA
 #' @example TaqMan chemistry data set
-#' colnames() : only if you want to rename the first three columns that by default are "Name", "Name.1" and "Type.1"
+#' colnames() : only if you want to rename the first three columns that by default are "Name", "Type", "Name.1" and "Type.1"
 
-taq_data <- read.csv("file.csv", skip = 11, sep = ",", dec = ".", header = TRUE, fill = TRUE, colClasses = c("NULL", "character", "NULL", "NULL", "character", "character", "numeric", "NULL", "NULL", "character", "NULL", "NULL"), na.strings = "999")
+taq_data <- read.csv("file.csv", skip = 11, sep = ",", dec = ".", header = TRUE, fill = TRUE, colClasses = c("NULL", "character", "character", "numeric", "character", "character", "numeric", "NULL", "NULL", "character", "NULL", "NULL"), na.strings = "999")
 
-colnames(taq_data) <- c("sampleID", "geneID", "reference", "ct", "status")
+colnames(taq_data) <- c("sampleID", "type", "concentration", "geneID", "reference", "ct", "status")
 
 #'
 #' @example EvaGreen chemistry data set (more columns with melting curve data)
 
-eva_data <- read.csv("file.csv", skip = 11, sep = ",", dec = ".", header = TRUE, fill = TRUE, colClasses = c("NULL", "character", "NULL", "NULL", "character", "character", "numeric", "NULL", "NULL", "character", "NULL", "NULL","NULL", "NULL", "NULL"), na.strings = "999")
+eva_data <- read.csv("file.csv", skip = 11, sep = ",", dec = ".", header = TRUE, fill = TRUE, colClasses = c("NULL", "character", "character", "numeric", "character", "character", "numeric", "NULL", "NULL", "character", "NULL", "NULL","NULL", "NULL", "NULL"), na.strings = "999")
 
-colnames(eva_data) <- c("sampleID", "geneID", "reference", "ct", "status")
-
-
-
-#' If you have added the efficiency values in the last column (see pre_process_data.txt), you can add one item in colClasses
-#'
-#'
-#' @example TaqMan chemistry data set
-#' @example EvaGreen chemistry data set
-
-taq_data <- read.csv("file.csv", skip = 11, sep = ",", dec = ".", header = TRUE, fill = TRUE, colClasses = c("NULL", "character", "NULL", "NULL", "character", "character", "numeric", "NULL", "NULL", "character", "NULL", "NULL", "numeric"), na.strings = "999")
-colnames(taq_data) <- c("sampleID", "geneID", "reference", "ct", "status", "efficiency")
-
-eva_data <- read.csv("file.csv", skip = 11, sep = ",", dec = ".", header = TRUE, fill = TRUE, colClasses = c("NULL", "character", "NULL", "NULL", "character", "character", "numeric", "NULL", "NULL", "character", "NULL", "NULL","NULL", "NULL", "NULL", "numeric"), na.strings = "999")
-colnames(eva_data) <- c("sampleID", "geneID", "reference", "ct", "status", "efficiency")
+colnames(eva_data) <- c("sampleID", "type", "concentration", "geneID", "reference", "ct", "status")
 
 
 #' It is possible that the Fluidigm software calculates a Ct value but gives it a Fail status.
