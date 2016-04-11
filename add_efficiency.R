877293c47ea81dcc7b4cde39c6f046c562087b87
Juliette Pouch <pouch@biologie.ens.fr>
2016-04-11 12:16:37 +0200

    Update add_efficiency.R
    Change NULL to NA function added
    missing parameter added to lookup function

--- add_efficiency.R
+++ add_efficiency.R
@@ -1,31 +1,12 @@
 #! /usr/bin/Rscript
 
 ### Add efficiency values to data set ###
-#' 
-#' Create subset table with Standard samples only
-#'
-#' @param taq_data= data frame to be processed
-#' @param taq_data$type == "Standard": selects samples that are referenced as standard in the Sample Setup from Real-time Analysis software.
-#'
-#' @example
-#'
-#' Remove first columns containing sample name (no longer needed)
-
-standard <- subset(taq_data, taq_data$type == "Standard")
-standard[,1] <- NULL
-
-#' Set concentrations to log10
-#'
-#' @example
-
-
-standard$concentration <- log10(standard$concentration)
-
 #' Create matrix to be melted
 #'
 #' value: vector containing the Ct values
 #' genes: vector containing gene names
 #' conc: vector containing log(concentration)
+#' if there is a NA value in this vector, it has to be removed by selecting only the numeric values using the na.omit() function
 #'
 #' @param value: vector of values used to fill the matrix
 #' @param nrow: number of rows. Must be equal to the length of the vector used for rows, in our case length(slope) = 4
@@ -38,24 +19,25 @@ standard$concentration <- log10(standard$concentration)
 #'
 #' @example
 
-value <- standard[,3]
-genes <- unique(standard[,2])
-conc <- unique(standard[,1])
+value <- taq_data[taq_data$type == "Standard",5]
+genes <- unique(taq_data[,4])
+conc <- na.omit(log10(unique(taq_data[taq_data$type == "Standard",3])))
 
-mat <- matrix(value, nrow  = 4, ncol = 96, dimnames = list(slope, genes), byrow = TRUE)
+mat <- matrix(value, nrow  = 4, ncol = 96, dimnames = list(conc, genes), byrow = TRUE)
 mat <- as.data.frame(mat)
 mat$slope <- conc
 
 #' Melt the data frame (stacks a set of columns into a single column of data)
 #'
 #' @param mat: data frame to be processed
-#' @param id: specifies the id variables
-#' @param measure: specifies the measured variables
+#' @param id: specifies the id variables, in our case the log(concentration) stored in the last column
+#' @param measure: specifies the measured variables (all columns execpt the last one)
 #' @param variable: id to be stacked into a single column
+#' We use na.omit() to remove NA values that will generate an error when calling the by() function
 #'
 #' @example
 
-std <- melt(mat, id=names(mat)[97], measure=names(mat)[1:96], variable = "genes")
+std <- na.omit(melt(mat, id=names(mat)[97], measure=names(mat)[1:96], variable = "genes"))
 
 #' Extract slope value from linear regression
 #'
@@ -65,12 +47,24 @@ std <- melt(mat, id=names(mat)[97], measure=names(mat)[1:96], variable = "genes"
 #'
 #' @example
 #'
+#' Some genes may return NULL in the "fits" list.
+#' We need to change NULL to NA in order to prevent the difference in length error from the do.call function.
+#'
+#' create function to change NULL to NA
+#' apply function to list of interest
 #' create data frame with only gene names and slope value
 #' calculate efficiency with 10^(-1/slope) formula
 #'
 #' @example
 
 fits <- by(std, std$genes, function(i) coef(lm(value ~ slope, i)))
+
+nullToNA <- function(x) {
+  x[sapply(x, is.null)] <- NA
+  return(x)
+}
+fits <- nullToNA(fits)
+
 eff <- data.frame(genes = names(fits), do.call(rbind, fits))[c(1,3)]
 eff$efficiency <- 10^(-1/eff$slope)
 
@@ -78,9 +72,11 @@ eff$efficiency <- 10^(-1/eff$slope)
 #'
 #' @param taq_data$geneID: column containing value to be looked up
 #' @param eff: data frame containing values to be extracted and assigned
+#' @param missing: attributes NA to any value missing in the eff data (i. e. genes not present in the eff data frame)
 #'
 #' @example
-taq_data$efficiency <- lookup(taq_data$geneID, eff)
+
+taq_data$efficiency <- lookup(taq_data$geneID, eff, missing = NA)
 
 
 ### Optional ###
