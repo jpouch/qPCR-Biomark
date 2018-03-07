a631818e32b7146ad9f0a5fdf57635111f225570
Juliette Pouch <juliette.pouch@gmail.com>
2018-03-07 15:28:22 +0100

    Update pfaffl_method.R
    Change column number for gene names

--- normalization/pfaffl_method.R
+++ normalization/pfaffl_method.R
@@ -13,7 +13,7 @@
 #'
 #'definition of the function to calculate delta Ct between reference samples and experimental sample
 #' @param df: stands for data frame
-#' @param g1: column containing gene names, usually [2] when open_data.R has been followed
+#' @param g1: column containing gene names, usually [4] when open_data.R has been followed
 #' @param ct1: column containing Ct values, usually [6] when open_data.R has been followed
 #' @param e1: column containing efficiency values, usually [7] when open_data.R has been followed and status deleted
 #' @param WT: defines a subset data for the reference sample used (put your reference sample name in the quotes. Caution: R is case sensitive)
@@ -28,7 +28,7 @@
 #' @param taq_data: data frame to be processed
 #' @param 1: apply the function to each row
 #' @param function(row) delta_pfaffl: calls the function you want to apply
-#' @param row[2]: refers to the second column containing the gene names
+#' @param row[4]: refers to the second column containing the gene names
 #' @param row[6]: refers to the fourth column containing the ct values
 #' @param row[7]: refers to the fifth column containing the efficiency values
 #' 
@@ -45,7 +45,7 @@ delta_pfaffl <- function(df, g1, ct1, e1){
 }
 
 
-taq_data$delta <- apply(taq_data, 1, function(row) delta_pfaffl(taq_data, row[2], row[6], row[7]))
+taq_data$delta <- apply(taq_data, 1, function(row) delta_pfaffl(taq_data, row[4], row[6], row[7]))
 
 #' definition of the function to calculate delta ratio between target genes and reference gene
 #' @param df: stands for data frame
