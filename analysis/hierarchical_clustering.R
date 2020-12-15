2092fad2fe8092ed856410a1e4455a9217fafcb8
Juliette Pouch <juliette.pouch@gmail.com>
2020-12-15 14:05:21 +0100

    Update hierarchical_clustering.R

--- analysis/hierarchical_clustering.R
+++ analysis/hierarchical_clustering.R
@@ -1,69 +1,37 @@
 #! /usr/bin/Rscript
 
+
+
 ########## Hierarchical clustering ##########
 
 #' Generate vectors containing needed variables
 #'
-#' ct: contains Ct values (column 4)
-#' genes: contains gene names (unique occurences)
+#' norm_val: contains normalized values (column 9 or other depending on the normalization step)
+#' geneID: contains gene names (unique occurences)
 #' samples: contains sample names (unique occurences)
 #'
 #' @example 96.96 experiment (96 samples, 96 genes)
 
-ct <- taq_data[, 6]
+norm_val <- taq_data[, 9]
 
-genes <- unique(taq_data[, 4])
+geneID <- unique(taq_data$geneID)
 
-samples <- unique(taq_data[, 1])
+samples <- taq_data[1:96, 2]
 
 
 #' Generate matrix
 #' In this example, the clustering is made on the samples.
-#' If you want to look at the genes, you need to revert this matrix using t(mat_clust), or invert the order of the vectors in dimnames
+#' If you want to look at the genes, you need to revert this matrix using t(mat_clust)
 #'
-#' @param ct: values to be added to the matrix
+#' @param norm_val: values to be added to the matrix
 #' @param nrow: specifies the number of rows (should be equal to the length of the vector "samples". To get this number, length(samples))
-#' @param ncol: specifies the number of columns (should be equal to the length of the vector "genes". To get this number, length(genes))
+#' @param ncol: specifies the number of columns (should be equal to the length of the vector "geneID". To get this number, length(geneID))
 #' @param dimnames: list containing names to be given to rows and columns. First item is for rows, and second item is for columns
 #'
 #' @ example
 
-mat_clust <- matrix(ct, nrow = 96, ncol = 96, dimnames = list(samples, genes))
+mat_clust <- matrix(norm_val, nrow = 96, ncol = 96, dimnames = list(samples, geneID))
 
-
-#' Calculate median of the Ct values per gene
-#'
-#' @param mat_clust: matrix to be processed
-#' @param 2: apply function by column
-#' @param median: calculates median
-#' @param na.rm = TRUE: allows to calculate median even though your data contains NA values
-#'
-#' @ example
-
-medians <- apply(mat_clust, 2, median, na.rm = TRUE)
-
-
-#' Calculate mean average deviation of the Ct values per gene
-#'
-#' @param mat_clust: matrix to be processed
-#' @param 2: apply function by column
-#' @param mad: calculates mean average deviation
-#'
-#' @ example
-
-mads <- apply(mat_clust, 2, mad)
-
-
-#' Center each column using scale() function
-#' Each column has its median substracted from its valus and divided by its mean average deviation
-#'
-#' @param mat_clust: matrix to be processed
-#' @param center: specifies the value to center the data (substraction)
-#' @param scale: speficies the value to scale the data (division)
-#'
-#' @ example
-
-data.use <- scale(mat_clust, center = medians, scale = mads)
 
 
 #' Generate the distance matrix
@@ -73,7 +41,7 @@ data.use <- scale(mat_clust, center = medians, scale = mads)
 #'
 #' @example
 
-data.dist <- dist(data.use, method = "euclidian")
+data.dist <- dist(mat_clust, method = "euclidian")
 
 
 #' Generate cluster solution
@@ -92,7 +60,13 @@ data.hclust <- hclust(data.dist, method = "complete")
 #' @param labels: specifies the vector containing names of the clustered variable (i. e. samples)
 #' @param xlab: abscissa legend
 #' @param main: plot title
+#' @param hang: align labels
 #'
 #' @example
 
-plot(data.hclust, labels = samples, xlab = "", main = "Dendrogram")
+plot(data.hclust, labels = samples, xlab = "", main = "Dendrogram", hang = -1)
+
+
+
+
+## J. Pouch ## 2020 ##
